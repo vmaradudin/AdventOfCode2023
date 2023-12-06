9e112e2c042bd7222b539ff7cab75fa985583275
Vyacheslav Maradudin <95409936+vmaradudin@users.noreply.github.com>
2023-12-06 19:08:15 +0100

    Update Common.fs

--- src/AdventOfCode.Framework/Common.fs
+++ src/AdventOfCode.Framework/Common.fs
@@ -13,7 +13,7 @@ module Types =
 
     let private execute day puzzle1 puzzle2 =
         let start = System.DateTime.Now
-        (File.ReadAllLines ($@"inputs\Input{ day |> string|> _.PadLeft(2,'0')}.txt"))
+        (File.ReadAllLines ($@"Inputs/Input{ day |> string|> _.PadLeft(2,'0')}.txt"))
         |> fun i -> (day, (puzzle1 |> format i), (puzzle2 |> format i), (System.DateTime.Now - start).TotalMilliseconds)
         |> fun (d, p1, p2, t) ->  printfn "Day %2d | 1: %20s | 2: %20s | Time: %f ms" d p1 p2 t
 
