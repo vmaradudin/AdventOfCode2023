1c3495c733572b9cfc4e23bd483068f1791a5473
Vyacheslav Maradudin <vmaradudin@gmail.com>
2023-12-20 23:48:19 +0100

    20 wip

--- src/2023/Modules/Day20.fs
+++ src/2023/Modules/Day20.fs
@@ -1,125 +1,113 @@
 namespace AdventOfCode2023
 
 open Common.Types
+open Common.Math
 open System.Collections.Generic
 open System
 
 module Day20 =
+
     type Module =
         {
             name: string;
             prefix: char;
             state: bool;
-            receivers: string array;
-            memory: (string*bool) array;
+            recievers: string list;
+            senders: string list;
         }
-        member this.Recieve signal = 
+        member this.Recieve (broadband:Dictionary<string,Module>) signal = 
             match this.prefix, signal with
-            |'%', true  -> this, fun (br:Dictionary<string, Module>) ->  None
-            |'%', false -> {this with state = not this.state}, fun (br:Dictionary<string, Module>) -> this.state |> not |> Some
-            |'&', _ -> {this with state = not this.state}, fun (br:Dictionary<string, Module>) ->  
+            |'%', true  -> None
+            |'%', false -> this.state |> not |> Some
+            |'&', _ -> this.senders |> List.forall (fun s -> (broadband.Item s)|> _.state) |> not |> Some
+            |_ -> signal |> Some
+            |> fun f ->
+                broadband.Item this.name <- {this with state = f |> function |Some(x) -> x |_-> this.state}
+                match f with
+                |Some(x) -> this.recievers |> List.map(fun r -> r, x)
+                |None -> []
 
-
-    let cache = Dictionary<string, int array>()
     let broadband = Dictionary<string,Module>()
-    let Process (theModule:Module) ((sender,thePulse):string*bool) step =
-        match theModule.prefix, thePulse, theModule.state with
-        |'%', true,_ -> theModule, None
-        |'%', false, state ->  {theModule with state = not state}, state|>not |> Some
-        |'&', input, _ -> 
-            let newMemory = theModule.memory |> Array.map(fun (s, state) -> if s = sender then (s,input) else (s,state))
-            {theModule with memory = newMemory}, (newMemory|>Array.forall(snd) |> not |> Some)
-        |_, input, _ -> theModule, input |> Some
-        |> fun (newModuleState, output) -> 
-            newModuleState, 
-            match output with
-            |Some(x) -> 
-                if x && [|"ch";"gh";"sv";"th"|] |> Array.contains theModule.name
-                    then 
-                        if cache.ContainsKey(theModule.name) |> not then cache.Add(theModule.name, [||]) else ignore()
-                        cache.Item(theModule.name) <- (Array.append (cache.Item theModule.name) [|step|]) 
-                theModule.receivers |> Array.map(fun r -> r,(theModule.name,x))
-            |_ -> Array.empty
-
+    let listener = Dictionary<bool, int>()
+    let buttonSignal = ("broadcaster", false)
 
     let parse (input:string array) =
         let networkMap = 
             input
-            |> Array.map _.Split([|" -> "; ", "|], StringSplitOptions.RemoveEmptyEntries)
+            |> List.ofArray
+            |> List.map (fun l -> l.Split([|" -> "; ", "|], StringSplitOptions.RemoveEmptyEntries) |> List.ofArray)
+
         let network =
             networkMap
-            |> Array.map (fun l ->
-                match l[0].[0], l[0], l|>Array.tail with
-                |'%', name, receivers -> name.Substring(1), {name = name.Substring(1); prefix = '%'; state = false; receivers = receivers; memory = Array.empty}
-                |'&', name, receivers -> name.Substring(1), {name = name.Substring(1); prefix = '&'; state = false; receivers = receivers; 
-                    memory = 
-                        networkMap
-                        |> Array.filter(fun a -> 
-                            a|>Array.tail 
-                            |> Array.contains (name.Substring(1))) 
-                            |> Array.map( fun a -> a |> Array.head |> _.Trim([|'%';'&'|]), false)}
-                |_,name, receivers -> name, {name = name; prefix=' '; state = false; receivers = receivers; memory = Array.empty})
-        network
-
-    let button = ("broadcaster",("button", false))
-    let ToString (m:Module) =
-        $"name={m.name} | state:{m.state} | memory:{(m.memory |> Array.map(fun (a,b) -> a + b.ToString()+ '|'.ToString()) |> String.concat(String.Empty))}"
-
-    let rec processSignals (modules:Module array) (counter:(bool*int) array) (signals: (string*(string*bool)) array) (getHitTo:string) (step:int) =
-        if signals.Length = 0
-        then 
-            modules, counter, false
-        else
-        let (receiver, (sender, pulse)) = signals |> Array.head
-        match modules |> Array.tryFind(fun m -> m.name = receiver) with
-        |Some(receiverModule) ->
-            let (newRecieverState, outputs) = Process receiverModule (sender, pulse) step
-            if getHitTo <> "" && outputs |> Array.exists( fun (r,(_,p)) ->  getHitTo = r && p = false) 
-            then 
-                modules, counter, true
-            else
-            let newCounter = counter|>Array.map (fun (s,c) -> s, (if s = pulse then c + 1 else c))
-            processSignals (modules |> Array.map(fun m -> if m.name <> receiver then m else newRecieverState)) newCounter (Array.append (signals |> Array.tail) outputs) getHitTo step
-        |_ -> 
-            let newCounter = counter|>Array.map (fun (s,c) -> s, (if s = pulse then c + 1 else c))
-            processSignals (modules) newCounter (signals |> Array.tail) getHitTo step
-
-    let player (modules:Module array, counter:(bool*int) array, getHitTo, stopped) (signals: (string*(string*bool)) array) =
-        let (nModules, nCounter, stopped) = processSignals modules counter signals getHitTo 0
-        nModules, nCounter, getHitTo, stopped
-
-    let rec player2 (modules:Module array, counter:(bool*int) array, getHitTo, stopped, i) (signals: (string*(string*bool)) array) =
-        let (nModules, nCounter, stopped) = processSignals modules counter signals getHitTo i
-        if stopped then
-            i
-        else
-            player2 (nModules, nCounter, getHitTo, stopped, i + 1) signals
-    let puzzle1 (input:string array) = 
-        let modules = input |> parse |> Array.unzip |> snd
-        seq{for i in 1..1000 ->  [|("broadcaster",("button", false))|]} |> Seq.fold player (modules, [|(true,0);(false,0)|], "",false)
-        |> fun (_, counter,_,_) -> counter |> Array.unzip |> snd |> Array.map int64 |> Array.fold (*) 1L
+            |> List.map (fun l ->
+                let title::recievers = l
+                let label = title.Trim([|'%';'&'|])
+                let prefix = title[0] |> function |x when x = '%' ||x = '&' -> x |_-> ' '
+                {
+                    name = label;
+                    prefix = prefix;
+                    state = false;
+                    recievers = recievers;
+                    senders = networkMap |> List.choose(fun (h::t) -> if (t|> List.contains label) then (h.Trim([|'%';'&'|]) |> Some) else None);
+                })
+        broadband.Clear();
+        network |> List.iter(fun m -> broadband.Add(m.name, m))
+        listener.Clear();
+        listener.Add(false, 0)
+        listener.Add(true, 0)
+        broadband
     
-
-    let rec lcmMultiple (numbers :int64 array) =
-        let rec gcd x y =
-            match y with
-            |0L -> x
-            |_ -> gcd y (x % y)
-
-        let lcm a b = a*b/(gcd a b)
-        
-        if numbers.Length = 1 
-        then 
-            numbers[0]
+    let rec processSignals steps (signals:(string*bool) list) =
+        if signals = []
+        then steps
         else
-            let newV = lcm numbers[0] numbers[1]
-            numbers |> Array.skip 2 |> Array.append [|newV|] |> lcmMultiple
+        let signal::restOfSignals = signals
+        let (moduleName, pulse) = signal
+        listener.Item(pulse) <- listener.Item(pulse) + 1
+        let output =
+            if broadband.ContainsKey moduleName 
+            then
+                broadband.Item(moduleName).Recieve(broadband) pulse
+            else
+                []
+        processSignals (steps + 1) (restOfSignals @ output)
+
+    let getRequiredSenders moduleName =
+        let senders = 
+            [for entry in broadband -> if (entry.Value.recievers |> List.contains moduleName) then Some(entry.Key) else None]
+            |> List.choose id
+            |> List.map (fun s -> broadband.Item(s))
+        if (senders|> List.exists (fun t -> t.prefix <> '&')) then failwith("Unexpected Input!")
+        senders |> List.collect _.senders
+
+    let rec calculateStepsNumber buttonHits (state:(string*(int option)*(int option)) list) =
+        if state |> List.forall(fun (a,b,c) -> b.IsSome && c.IsSome)
+        then 
+            state|> List.map(fun (_,v1, v2) -> v2.Value - v1.Value |> int64) |> Array.ofList |> arrayLCM
+        else
+            processSignals 0 [buttonSignal] |> ignore
+            state
+            |> List.map (fun (mName, firstHit, secondHit) ->
+                match broadband.Item(mName).state, firstHit, secondHit with
+                | true, None, _ -> (mName, Some(buttonHits), None)
+                | true, Some(x), None -> (mName, Some(x), Some(buttonHits))
+                |_ -> mName, firstHit, secondHit)
+            |> calculateStepsNumber (buttonHits + 1)
+    
+    let calculateWhenFalseRecievedBy moduleName =
+        getRequiredSenders moduleName
+        |> List.map (fun l -> l,None,None)
+        |> calculateStepsNumber 1
+        
+
+    let puzzle1 (input:string array) = 
+        parse(input) |> ignore
+        seq{for i in 1..1000 -> [buttonSignal]}
+        |> Seq.fold processSignals 0 |> ignore
+        listener.Values|> List.ofSeq |> List.map int64 |> List.fold (*) 1L
     
     let puzzle2 (input:string array) =
-        [|3917L;3943L;3947L;4001L|] |> lcmMultiple
-        //cache.Clear()
-        //let modules = input |> parse |> Array.unzip |> snd
-        //player2 (modules,[|(true,0);(false,0)|], "output", false, 0) [|button|]
-        
+        parse(input) |> ignore
+        calculateWhenFalseRecievedBy "rx"
                 
     let Solution = (new Solution(20, puzzle1, puzzle2) :> ISolution).Execute
---
+++ src/AdventOfCode.Framework/Math.fs
@@ -0,0 +1,21 @@
+﻿namespace Common
+
+open System.IO
+
+[<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute>]
+module Math =
+    let rec gcd x y =
+            match y with
+            |0L -> x
+            |_ -> gcd y (x % y)
+    
+    let LCM a b = a*b/(gcd a b)
+
+    let rec arrayLCM (numbers :int64 array) =
+        if numbers.Length = 1 
+        then 
+            numbers[0]
+        else
+            let newV = LCM numbers[0] numbers[1]
+            numbers |> Array.skip 2 |> Array.append [|newV|] |> arrayLCM
+
